8128cd60250d3fa82f3eddabe2dea2b56ac59eb9
Fleury <guilherme.fleury26@gmail.com>
2022-11-29 17:34:11 -0300

    adds firebase files
    changes main type

--- meau/lib/main.dart
+++ meau/lib/main.dart
@@ -1,7 +1,11 @@
 import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
